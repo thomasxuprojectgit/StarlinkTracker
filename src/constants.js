8304186e216aa92711a476541b8922f834dc5731
thomasxuprojectgit <107577761+thomasxuprojectgit@users.noreply.github.com>
2022-07-25 13:37:42 -0500

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
+
+export const BASE_URL = "https://54.145.179.110";
